9be1d3638dd0d5c6fe412e6a57a294ec24adc961
vrushtikaria <20it052@charusat.edu.in>
2022-06-13 00:00:13 +0530

    commit

--- src/Register.js
+++ src/Register.js
@@ -1,140 +1,243 @@
-import React from 'react';
-import DataService from './DataService';
+import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from './api/axios';
 
-class Register extends React.Component {
-constructor(props) {
-    super(props);
 
-    this.state = {
-        name : "",
-        email: "",
-        phone: "",
-        password: ""
-    };
-    this.changenamehandler = this.changenamehandler.bind(this);
-    this.changemailhandler = this.changemailhandler.bind(this);
-    this.changephonehandler = this.changephonehandler.bind(this);
-    this.changepasshandler = this.changepasshandler.bind(this);
-}
+const name_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const password_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const EMAIL_REGEX=/^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+const PHN_REGEX=/^(\+\d{1,3}[- ]?)?\d{10}$/;
 
-    saveUser = (e) => {
-        if(e && e.preventDefault) {
-            e.preventDefault();
-            e.persist();
-        }
 
-        let user = {name: this.state.name, email: this.state.email, phone:this.state.phone, password:this.state.password};
-        console.log('user => '+JSON.stringify(user));
+const Register = () => {
+    const nameRef = useRef();
+    const errRef = useRef();
 
-        DataService.createUser(user).then(Response => {
-            console.log(Response);
-            let res = JSON.stringify(Response);
-            console.log("user id : ", Response.id);
-            
-            DataService.sendOTP(res.id).then(Resp => {
-                console.log(Resp);
-            })
-        })
+    const [name, setname] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [nameFocus, setnameFocus] = useState(false);
 
-        // "a81c54b9-b10d-428f-b721-9398a97af022"
-        //let Uid = {uid : Object.uid}
+    const [email,setEmail] = useState('');
+    const [validemail, setValidEmail] = useState(false);
+    const [EmailFocus, setEmailFocus] = useState(false);
+
+    const [phone,setphone] = useState('');
+    const [validphone, setValidphone] = useState(false);
+    const [phoneFocus, setphoneFocus] = useState(false);
+    
+    const [password, setpassword] = useState('');
+    const [validpassword, setValidpassword] = useState(false);
+    const [passwordFocus, setpasswordFocus] = useState(false);
+
+    const [matchpassword, setMatchpassword] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        nameRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setValidName(name_REGEX.test(name));
+    }, [name])
+
+    useEffect(() => {
+        setValidEmail(EMAIL_REGEX.test(email));
+    }, [email])
+
+    useEffect(() => {
+        setValidphone(PHN_REGEX.test(phone));
+    }, [phone])
+
+    useEffect(() => {
+        setValidpassword(password_REGEX.test(password));
+        setValidMatch(password === matchpassword);
+    }, [password, matchpassword])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [name, password, matchpassword])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         
+        const v1 = name_REGEX.test(name);
+        const v2 = password_REGEX.test(password);
+        if (!v1 || !v2) {
+            setErrMsg("Invalid Entry");
+            return;
+        }
+        try {
+            let a={name,phone,email,password}
+            const response = await axios.post('https://api.shilpimultiplex.com/api/Auth/CreateUser',a)
+            //      JSON.stringify({ name,phone, email,password }),
+            //      {
+            //          headers: { 'Content-Type': 'application/json' },
+            //         withCredentials: true
+                    
+            //     }
+                
+            // );
+            
+            console.log(response?.data);
+            console.log(response?.accessToken);
+            console.log(JSON.stringify(response))
+            setSuccess(true);
+            
+            setname('');
+            setpassword('');
+            setEmail(' ');
+            setphone(' ');
+            setMatchpassword('');
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 409) {
+                setErrMsg('namename Taken');
+            } else {
+                console.log(name);
+                console.log(phone);
+                console.log(email);
+                console.log(password);
+                setErrMsg('Registration Failed')
+            }
+            errRef.current.focus();
+        }
     }
 
-    changenamehandler = (event) => {
-        this.setState({name: event.target.value})
-    }
-
-    changemailhandler = (event) => {
-        this.setState({email: event.target.value})
-    }
-
-    changephonehandler = (event) => {
-        this.setState({phone: event.target.value})
-    }
-
-    changepasshandler = (event) => {
-        this.setState({password: event.target.value})
-    }
-
-    render() {
-        return(
-            // <div>
-            //     <h1>Register Page</h1>
-            //     <input type="text" value={this.state.name} name="name" onChange = {this.changenamehandler} placeholder="Name"/><br />
-            //     <input type="email" value={this.state.email} name="email" onChange = {this.changemailhandler} placeholder="Email Id"/><br />
-            //     <input type="number" value={this.state.phone} name="phone" onChange = {this.changephonehandler} placeholder="Mobile Number"/><br />
-            //     <input type="password" value={this.state.password} name="password" onChange = {this.changepasshandler} placeholder="Password" /><br />
-            //     <button onClick={this.saveUser}>Sing Up</button>
-            // </div>
-
-            <div>
+    return (
+        <>
+            {success ? (
                 <section>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='card-body'>
-                            <form>
-                                <div className='form-group'>
-                                    <label>Name : </label>
-                                    <br />
-                                    <input
-                                    type='text'
-                                    autoComplete="off"
-                                    placeholder='Name'
-                                    name="name"
-                                    className='form-control'
-                                    value={this.state.name}
-                                    onChange={this.changenamehandler} />
-                                </div>
-                                <p> </p>
-                                <div className='form-group'>
-                                    <label>Email : </label>
-                                    <br />
-                                    <input
-                                    type='text'
-                                    autoComplete="off"
-                                    placeholder='Email'
-                                    name="email"
-                                    className='form-control'
-                                    value={this.state.email}
-                                    onChange={this.changemailhandler} />
-                                </div>
-                                <p> </p>
-                                <div className='form-group'>
-                                    <label>Phone : </label>
-                                    <br />
-                                    <input
-                                    type='text'
-                                    autoComplete="off"
-                                    placeholder='phone'
-                                    name="phone"
-                                    className='form-control'
-                                    value={this.state.phone}
-                                    onChange={this.changephonehandler} />
-                                </div>
-                                
-                                <p> </p>
-                                <div className='form-group'>
-                                    <label>Password : </label>
-                                    <br />
-                                    <input type = "password"
-                                    placeholder='password'
-                                    autoComplete="off"
-                                    name="password"
-                                    className='form-control'
-                                    value={this.state.password}
-                                    onChange={this.changepasshandler} />
-                                </div>
-                                <p> </p>
-                                <div className="text-center">
-                                    <button className="btn btn-success" onClick={this.saveUser}>Create user</button>
+                    <h1>Success!</h1>
+                    <p>
+                        <a href="#">Sign In</a>
+                    </p>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                    <h1>Register</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="namename">
+                            username:
+                            <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validName || !name ? "hide" : "invalid"} />
+                        </label>
+                        <input
+                            type="text"
+                            id="namename"
+                            ref={nameRef}
+                            autoComplete="off"
+                            onChange={(e) => setname(e.target.value)}
+                            value={name}
+                            required
+                            aria-invalid={validName ? "false" : "true"}
+                            aria-describedby="uidnote"
+                            onFocus={() => setnameFocus(true)}
+                            onBlur={() => setnameFocus(false)}
+                        />
+                        <p id="uidnote" className={nameFocus && name && !validName ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            4 to 24 characters.<br />
+                            Must begin with a letter.<br />
+                            Letters, phonebers, underscores, hyphens allowed.
+                        </p>
 
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <p>
+
+                        <label htmlFor="password">
+                            Password:
+                            <FontAwesomeIcon icon={faCheck} className={validpassword ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validpassword || !password ? "hide" : "invalid"} />
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={(e) => setpassword(e.target.value)}
+                            value={password}
+                            required
+                            aria-invalid={validpassword ? "false" : "true"}
+                            aria-describedby="passwordnote"
+                            onFocus={() => setpasswordFocus(true)}
+                            onBlur={() => setpasswordFocus(false)}
+                        />
+                        <p id="passwordnote" className={passwordFocus && !validpassword ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            8 to 24 characters.<br />
+                            Must include uppercase and lowercase letters, a phoneber and a special character.<br />
+                            Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                        </p>
+
+
+                        <label htmlFor="confirm_password">
+                            Confirm Password:
+                            <FontAwesomeIcon icon={faCheck} className={validMatch && matchpassword ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validMatch || !matchpassword ? "hide" : "invalid"} />
+                        </label>
+                        <input
+                            type="password"
+                            id="confirm_password"
+                            onChange={(e) => setMatchpassword(e.target.value)}
+                            value={matchpassword}
+                            required
+                            aria-invalid={validMatch ? "false" : "true"}
+                            aria-describedby="confirmnote"
+                            onFocus={() => setMatchFocus(true)}
+                            onBlur={() => setMatchFocus(false)}
+                        />
+                        <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            Must match the first password input field.
+                        </p>
+
+                        <label htmlFor="email">Email:
+                        <FontAwesomeIcon icon={faCheck} className={validemail ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validemail || !email ? "hide" : "invalid"} /></label>
+                        <input type="text" 
+                            id="email" 
+                            autoComplete="off"
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            required
+                            aria-invalid={validemail ? "false" : "true"}
+                            aria-describedby="uidnote"
+                            onFocus={() => setEmailFocus(true)}
+                            onBlur={() => setEmailFocus(false)}     
+                        />
+                        <p id="uidnote" className={EmailFocus && email && !validemail ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            <br />
+                            Enter Valid Email id
+                        </p>
+
+                          <label htmlFor="phn">phonenumber:
+                        <FontAwesomeIcon icon={faCheck} className={validphone ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validphone || !phone ? "hide" : "invalid"} /></label>
+                        <input type="text" 
+                            id="phone" 
+                            autoComplete="off"
+                            onChange={(e) => setphone(e.target.value)}
+                            value={phone}
+                            required
+                            aria-invalid={validphone ? "false" : "true"}
+                            aria-describedby="uidnote"
+                            onFocus={() => setphoneFocus(true)}
+                            onBlur={() => setphoneFocus(false)}
+                        />
+                        <p id="uidnote" className={phoneFocus && phone && !validphone ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            <br />
+                            Enter 10 digit Valid phoneber
+                        </p>
+                        
+
+                        <button disabled={!validName||!validemail||!validphone|| !validpassword || !validMatch ? true : false}>Sign Up</button>
+                    </form>
+                    <p>
                         Already registered?<br />
                         <span className="line">
                             {/*put router link here*/}
@@ -142,10 +245,9 @@ constructor(props) {
                         </span>
                     </p>
                 </section>
-            </div>
-        )
-    }
+            )}
+        </>
+    )
 }
-
 
 export default Register
